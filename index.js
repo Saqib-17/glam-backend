1c28b8806647789c6e9dc776d4d87e009bd3921b
Saqib-17 <sakib17shahidul@gmail.com>
2025-01-03 15:32:11 +0600

    Add CORS configuration and Vercel deployment settings

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin:[
+    "http://localhost:5173",
+    "https://glam-frontend-silk.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB URI and Client
