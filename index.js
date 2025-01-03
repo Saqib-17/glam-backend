629fb5e0be776ece92a6b74f0d1bafa74b378f79
Saqib-17 <sakib17shahidul@gmail.com>
2025-01-03 19:04:51 +0600

    Remove commented-out MongoDB connection and route definitions from index.js

--- index.js
+++ index.js
@@ -27,51 +27,6 @@ const client = new MongoClient(uri, {
   },
 });
 
-// async function run() {
-//   try {
-//     // Connect to MongoDB
-//     // await client.connect();
-//     console.log("Connected to MongoDB");
-
-//     // Reference to the `products` collection
-//     const productCollection = client.db("GlamKey").collection("products");
-//     const categoryCollection = client.db("GlamKey").collection("category");
-//     const trendingOffersCollection = client.db("GlamKey").collection("trendingOffers");
-//     const jewelleryGuidesCollection = client.db("GlamKey").collection("jewelleryGuides");
-
-//     // Route to fetch all products
-//     app.get('/products', async(req, res)=>{
-//         const result = await productCollection.find().toArray()
-//         res.send(result)
-//     })
-
-//     // Route to fetch all category
-//     app.get('/category', async(req, res)=>{
-//         const result = await categoryCollection.find().toArray()
-//         res.send(result)
-//     })
-
-//     // Route to fetch all trending offers
-//     app.get('/trendingOffers', async(req, res)=>{
-//         const result = await trendingOffersCollection.find().toArray()
-//         res.send(result)
-//     })
-
-//     // Route to fetch all jewellery guides
-//     app.get('/jewelleryGuides', async(req, res)=>{
-//         const result = await jewelleryGuidesCollection.find().toArray()
-//         res.send(result)
-//     })
-
-//     // Ping the database
-//     // await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged MongoDB successfully.");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error.message);
-//     process.exit(1);
-//   }
-// }
-
 async function run() {
   try {
     console.log("Connected to MongoDB");
